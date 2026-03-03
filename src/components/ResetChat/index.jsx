7da73886dc3419ae0e20924c5f6e1c4221a9acdb
Timo Schlatter <tds29@posteo.net>
2026-03-03 17:31:06 +0100

    Style delete chat button

--- src/components/ResetChat/index.jsx
+++ src/components/ResetChat/index.jsx
@@ -13,8 +13,10 @@ export default function ResetChat({ setChatHistory, settings, sessionId, resetSe
   return (
     <div className="allm-w-full allm-flex allm-justify-center allm-gap-x-1 p-0">
       <button
-        style={{ color: "#7A7D7E" }}
-        className="allm-h-fit allm-px-0 hover:allm-cursor-pointer allm-border-none allm-text-sm allm-bg-transparent hover:allm-opacity-80 hover:allm-underline"
+        style={{ color: "#06689A" }}
+        className="allm-h-fit allm-px-0 hover:allm-cursor-pointer allm-border-none allm-text-sm allm-bg-transparent hover:allm-text-[#C31A1A] hover:allm-underline"
+        onMouseEnter={(e) => (e.currentTarget.style.color = "#C31A1A")}
+        onMouseLeave={(e) => (e.currentTarget.style.color = "#06689A")}
         onClick={() => handleChatReset()}
       >
         {settings.resetChatText || t("chat.reset-chat")}
